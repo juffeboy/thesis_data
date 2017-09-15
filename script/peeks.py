66e4b3c69f8ebfcf178bbd149d31b21746f9c64a
Johan <juffeboy@gmail.com>
2017-09-15 09:46:19 +0000

    corrected mistakes

--- script/peeks.py
+++ script/peeks.py
@@ -2,9 +2,14 @@ import subprocess
 import sys
 from subprocess import Popen, PIPE
 import ast
+import datetime 
 import time
+from os.path import expanduser
 
-ccnl_path = "/home/johanc/ccn-lite/bin/ccn-lite-peek"
+home_path = expanduser("~")
+
+ccnl_path = home_path + "/ccn-lite/bin/ccn-lite-peek"
+#ccnl_path = "/home/johanc/ccn-lite/bin/ccn-lite-peek"
 sensor_address = "fd02::212:4b00:7a8:7185/1001"
 content_path = "s"
 mote_interval = 3
@@ -53,10 +58,10 @@ def get_latest_seq(start_seq):
 	return seq
 
 def trim_req_time(seq):
-	start_time = datetime.now()
-	current_sent_time = datetime.now()
+	start_time = datetime.datetime.now()
+	current_sent_time = datetime.datetime.now()
 	prev_sent_time = current_sent_time
-	total_time_spent = datetime.now()
+	total_time_spent = datetime.datetime.now()
 	
 	timeouts = 0
 	
@@ -66,11 +71,11 @@ def trim_req_time(seq):
 	sent_number_of_requests = 0
 	print("try to trim request time: ")
 	while True:
-		current_sent_time = datetime.now()
+		current_sent_time = datetime.datetime.now()
 		s_peek = send_peek(seq, temp_timeout)
 		sent_number_of_requests = sent_number_of_requests + 1
 		p_peek = process_peek(s_peek)
-		check_peek = int(p_peek)
+		check_peek = int(float(p_peek))
 		if (check_peek == -1):
 			#do something
 			timeouts = timeouts + 1
@@ -79,22 +84,24 @@ def trim_req_time(seq):
 			elapsed_time_to_ok = current_sent_time - prev_sent_time 	
 			total_time_spent = current_sent_time - start_time
 			break
+
 	print("time elapsed between timeout and ok: ", elapsed_time_to_ok)
 	print("Total time spend from start to ok: ", total_time_spent) 	
 	return elapsed_time_to_ok
 			
 
 
-def regular_request_intervall(start_seq):
+def regular_request_interval(start_seq):
 	seq = start_seq
 	sent_under_intervall = 0
 	rel_time = mote_interval
 	updated_rel_time = 0.0
 	trim_time = 0
+	print("Starting regular interval")
 	while True:
 		s_peek = send_peek(seq, mote_interval)
 		p_peek = process_peek(s_peek)
-		check_peek = int(p_peek)
+		check_peek = int(float(p_peek))
 		if (check_peek == -1):
 			#do something?
 			print("seqence: " + str(seq) + " got timeout")
@@ -105,17 +112,19 @@ def regular_request_intervall(start_seq):
 			
 		## update...	
 		if (seq%5==0): # for every fifth
-			trim_time = trim_request_interval(seq)
+			trim_time = trim_req_time(seq)
 			seq = seq + 1
+			print("seqence: " + str(seq) + " took: " + str(trim_time) + ". But was trimmed.")
 			## print to log!!
 			#updated_rel_time = mote_interval - trim_request_intervall(seq)
 
 		## sleep
-		time.sleep(mote_intervall)
+		time.sleep(mote_interval)
 
 def main():
 	latest_seq = get_latest_seq(1)
 	print("The latest SEQ is.. : ", latest_seq)
+	regular_request_interval(latest_seq)
 
 
 if __name__ == '__main__':
